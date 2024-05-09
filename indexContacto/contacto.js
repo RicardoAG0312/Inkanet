edd7fcd1967f8f1b57bf370fe9604c12740d9e46
Ricardo Goicochea <ricardo41216383@gmail.com>
2024-05-08 23:05:49 -0500

    CAMBIOS
    AGREGUE MEJOR RESPONSIVE E HICE MODIFICACIONES DE ALGUNAS SECCIONES

--- indexContacto/contacto.js
+++ indexContacto/contacto.js
@@ -1,19 +1,19 @@
 const enviarDatos = (event) => {
-    console.log("La función enviarDatos se ha llamado");
     event.preventDefault(); 
     let nombreIngresado = document.getElementById("nombre").value;
     let emailIngresado = document.getElementById("email").value;
     let celularIngresado = document.getElementById("celular").value;
     let comentarioIngresado = document.getElementById("comentario").value;
-
     if (nombreIngresado === "" || emailIngresado === "" || celularIngresado === "" || comentarioIngresado === "") {
-        alert("Todos los campos deben estar llenos");
-    } else if (isNaN(celularIngresado)) {
-        alert("El campo de número no debe contener letras");
+        Swal.fire({
+            title: "Información Incompleta",
+            text: "Para proceder con la solicitud de información se debe completar todos los campos",
+            icon: "warning"
+        });
     } else {
         Swal.fire({
             title: "Información enviada con éxito",
-            text: "You clicked the button!",
+            text: "Nos pondremos en contacto contigo lo más pronto posible.",
             icon: "success"
         });
         document.getElementById("nombre").value = "";
